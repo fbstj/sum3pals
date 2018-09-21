9f02a8fabd5b005dee353dd42de852063c14bde1
Joe ST <joe@fbstj.net>
2018-09-21 10:09:08 +0100

    [clsasification] document the 13 types of number +Small - described in section 2.2 - only detect on nubmers with enough digits (respecting section 2.1)

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,82 @@ struct Digits {
 	base: Digit,
 }
 
+/// classification of digits
+#[derive(Debug)]
+enum Class {
+	/// numbers with <7 digits
+	Small,
+	/// numbers of type A.1
+	/// - second most significant digit is >2
+	/// - D(least-sig  - most-sig - 2nd-most-sig + 1) != 0
+	TypeA1,
+	/// numbers of type A.2
+	/// - second most significant digit is >2
+	/// - D(least-sig  - most-sig - 2nd-most-sig digit + 1) == 0
+	TypeA2,
+	/// numbers of type A.3
+	/// - second most significant digit is {0,1,2}
+	/// - most significant digit not 1
+	/// - D(least-sig - most-sig + 2) != 0
+	TypeA3,
+	/// numbers of type A.4
+	/// - second most significant digit is {0,1,2}
+	/// - most significant digit not 1
+	/// - D(least-sig - most-sig + 2) == 0
+	TypeA4,
+	/// numbers of type A.5
+	/// - most-sig digits are 10
+	/// - 3rd-most-sig {0,1,2,3}
+	/// - D(least-sig - 3rd-most-sig) != 0
+	TypeA5,
+	/// numbers of type A.6
+	/// - most-sig digits are 10
+	/// - 3rd-most-sig {0,1,2}
+	/// - D(least-sig - 3rd-most-sig) == 0
+	TypeA6,
+	/// numbers of type B.1
+	/// - most-sig digit is 1
+	/// - 2nd-most-sig {0,1,2}
+	/// - 3rd-most-sig not {0,1,2,3}
+	/// - D(least-sig - 3rd-most-sig) != 0
+	TypeB1,
+	/// numbers of type B.2
+	/// - most-sig digit is 1
+	/// - 2nd-most-sig {0,1,2}
+	/// - 3rd-most-sig not {0,1,2}
+	/// - D(least-sig - 3rd-most-sig) == 0
+	TypeB2,
+	/// numbers of type B.3
+	/// - most-sig digit is 1
+	/// - 2nd-most-sig digit is {1,2}
+	/// - 3rd-most-sig digit is {0,1}
+	/// - lesat-sig is 0
+	TypeB3,
+	/// numbers of type B.4
+	/// - most-sig digit is 1
+	/// - 2nd-most-sig digit is {1,2}
+	/// - 3rd-most-sig digit is {2,3}
+	/// - lesat-sig is 0
+	TypeB4,
+	/// numbers of type B.5
+	/// - most-sig digit is 1
+	/// - 2nd-most-sig digit is {1,2}
+	/// - 3rd-most-sig digit is {0,1,2}
+	/// - least-sig not 0
+	TypeB5,
+	/// numbers of type B.6
+	/// - most-sig digit is 1
+	/// - 2nd-most-sig digit is {1,2}
+	/// - 3rd-most-sig digit is 3
+	/// - D(least-sig - 3) not 0
+	TypeB6,
+	/// numbers of type B.7
+	/// - most-sig digit is 1
+	/// - 2nd-most-sig digit is {1,2}
+	/// - 3rd-most-sig digit is 3
+	/// - lesat-sig is 3
+	TypeB7,
+}
 impl Digits {
 	fn from(mut num: u128, base: Digit) -> Digits {
 		let b : u128 = base as u128;
@@ -28,6 +104,13 @@ impl Digits {
 
 		Digits { digs: digits, base }
 	}
+
+	fn classify(&self) -> Class {
+		if self.digs.len() < 7 {
+			return Class::Small;
+		}
+		unimplemented!();
+	}
 }
 
 fn main() {
@@ -37,4 +120,5 @@ fn main() {
 				.expect("Please provide number to decompose into palindromes");
 	let digits = Digits::from(num, BASE);
     println!("{:?}", digits);
+    println!("{:?}", digits.classify());
 }
